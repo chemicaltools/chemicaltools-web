ac377d20be9291398fd1ce25decf6458d4020cd8
njzjz <njzjz@qq.com>
2016-10-16 00:50:19 +0800

    about

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>关于 -- 化学e+</title>
+<?php include 'head.php';?>
+  </head>
+  <body>
+<?php include 'header.php';?>
+    <section class="main-content">		
+<?php
+require 'load.php';
+include 'title.php';
+?>
+<h2>关于</h2>
+<p>作者：曾晋哲、陈子晗、邓凌雪</p>
+<p>联系我们：njzjz@msn.com</p>
+<p><a href="https://github.com/njzjz/Chemical-Tools-for-php">访问Github</a></p>
+<h3>技术支持</h3>
+<p>数据存储：<a href="https://leancloud.cn/">LeanCloud</a></p>
+<p>主题：<a href="https://github.com/jasonlong/cayman-theme">Cayman</a></p>
+<?php
+include 'foot.php';?>
+    </section>
+  </body>
+</html>
--- foot.php
+++ foot.php
@@ -1,3 +1,3 @@
 		<footer class="site-footer">		
-        <span class="site-footer-credits">本站由<a href="https://pages.github.com">GitHub Pages</a>使用<a href="https://twitter.com/jasonlong">Jason Long</a>创作的<a href="https://github.com/jasonlong/cayman-theme">Cayman主题</a>生成。</span>
+        <span class="site-footer-credits"><a href="about.php">关于</a>|<a href="http://chem.njzjz.win">下载软件</a>|<a href="https://github.com/njzjz">Github</a></span>
 		</footer>
