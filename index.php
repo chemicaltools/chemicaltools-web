e95a5b14dc1416760eb16a226fd3cc96fe2ddbd7
njzjz <njzjz@qq.com>
2016-10-16 17:49:55 +0800

    update index.php

--- index.php
+++ index.php
@@ -18,13 +18,23 @@ require 'doyouknow.php';
 <?php
 if ($currentUser != null) {
 	$element=$currentUser->get("historyElementOutputHtml");
+	if($element=="")$element="您尚未使用本功能，赶快试试吧！";
 	$mass=$currentUser->get("historyMassOutput");
+	if($mass=="")$mass="您尚未使用本功能，赶快试试吧！";
 	$acid=$currentUser->get("historyAcidOutput");
+	if($acid=="")$acid="您尚未使用本功能，赶快试试吧！";
 	$correct=(int)($currentUser->get("examCorrectNumber"));
 	$incorrect=(int)($currentUser->get("examIncorrectnumber"));
 	$sum=$correct+$incorrect;
 	$rate=(double)$correct/(double)$sum*100;
-	$exam= '共回答'.$sum.'题，其中'.$correct.'题正确，正确率为'.sprintf("%.2f", $rate).'%';
+	if($sum>0)$exam= '共回答'.$sum.'题，其中'.$correct.'题正确，正确率为'.sprintf("%.2f", $rate).'%';
+	else $exam="您尚未使用本功能，赶快试试吧！";
+}else{
+	$element='<a href="login.php">登陆</a>或<a href="signup.php">注册</a>后，即可查看历史记录，赶快试试吧！';
+	$mass='<a href="login.php">登陆</a>或<a href="signup.php">注册</a>后，即可查看历史记录，赶快试试吧！';
+	$acid='<a href="login.php">登陆</a>或<a href="signup.php">注册</a>后，即可查看历史记录，赶快试试吧！';
+	$exam='<a href="login.php">登陆</a>或<a href="signup.php">注册</a>后，即可存储战绩，赶快试试吧！';
+}
 ?>
 <h2><img src="/ico/blue_apple.png" width=30 height=30><a href="element.php">元素查询</a></h2>
 <p><?=nl2br($element)?></p>
@@ -35,7 +45,6 @@ if ($currentUser != null) {
 <h2><img src="/ico/orange_apple.png" width=30 height=30><a href="exam.php">元素记忆</a></h2>
 <p><?=nl2br($exam)?></p>
 <?php
-}
 include 'foot.php';?>
     </section>
   </body>
