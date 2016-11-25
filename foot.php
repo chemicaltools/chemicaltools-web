4a67433dcd044684b9f9d29cf1b6b6e67590f9ee
njzjz <njzjz@qq.com>
2016-11-25 16:28:44 +0800

    站点导航

--- foot.php
+++ foot.php
@@ -1,3 +1,3 @@
 		<footer class="site-footer">		
-        <span class="site-footer-credits"><a href="about.php">关于</a>|<a href="http://chem.njzjz.win">下载软件</a>|<a href="https://github.com/njzjz">Github</a></span>
+        <span class="site-footer-credits"><a href="http://www.njzjz.win/">站点导航</a>|<a href="about.php">关于</a>|<a href="http://chem.njzjz.win">下载软件</a>|<a href="https://github.com/njzjz">Github</a></span>
 		</footer>
