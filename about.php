70a40a7a44297decd8bbe723b7027f30e2eeb080
njzjz <njzjz@qq.com>
2017-02-19 16:53:37 +0800

    关于

--- about.php
+++ about.php
@@ -12,13 +12,16 @@
 require 'load.php';
 include 'title.php';
 ?>
-<h2>关于</h2>
+<h2>关于化学e+(网页版)</h2>
 <p>作者：曾晋哲、陈子晗、邓凌雪</p>
-<p>联系我们：njzjz@msn.com</p>
+<p>前端语言：Html/CSS、JavaScript</p>
+<p>后端语言：PHP</p>
+<p>联系我们：<a href="mailto:njzjz@msn.com">njzjz@msn.com</a></p>
 <p><a href="https://github.com/njzjz/Chemical-Tools-for-php">访问Github</a></p>
-<h3>技术支持</h3>
-<p>数据存储：<a href="https://leancloud.cn/">LeanCloud</a></p>
-<p>主题：<a href="https://github.com/jasonlong/cayman-theme">Cayman</a></p>
+<h3>第三方库</h3>
+<p>CSS：<a href="https://github.com/jasonlong/cayman-theme">Cayman</a></p>
+<p>JavaScript：<a href="https://jquery.com/">JQuery</a></p>
+<p>PHP：<a href="https://leancloud.cn/">LeanCloud</a></p>
 <?php
 include 'foot.php';?>
     </section>
