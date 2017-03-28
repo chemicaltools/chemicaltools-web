bf5168b346a431bd40e39210d3966dcc42d4ff1a
njzjz <njzjz@qq.com>
2017-03-28 19:34:09 +0800

    修正api

--- api.php
+++ api.php
@@ -1,3 +1,4 @@
 <?php
 header("Content-type: text/html; charset=utf-8"); 
-echo file_get_contents("https://chemapp.njzjz.win/".$_GET["type"].".php?ajax=1&html=no&input=".$_GET["input"]);
+$content=file_get_contents("https://chemapp.njzjz.win/".$_GET["type"].".php?ajax=1&html=no&input=".$_GET["input"]);
+if($_GET["method"]=="1")echo nl2br($content);else echo $content;
