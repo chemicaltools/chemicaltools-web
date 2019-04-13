170089e97bad6157afc3b010d5a3daa19af3db27
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-04-14 02:09:40 +0800

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -9,48 +9,5 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 library.add(faAtom, faBalanceScale, faFlask, faChartBar, faBurn, faQuestion, faCog)
 
 Vue.use(Vuetify, {
-  icons: {
-    'atom': {
-      component: FontAwesomeIcon,
-      props: {
-        icon: ['fas', 'atom']
-      }
-    },
-    'balance-scale': {
-      component: FontAwesomeIcon,
-      props: {
-        icon: ['fas', 'balance-scale']
-      }
-    },
-    'flask': {
-      component: FontAwesomeIcon,
-      props: {
-        icon: ['fas', 'flask']
-      }
-    },
-    'chart-bar': {
-      component: FontAwesomeIcon,
-      props: {
-        icon: ['fas', 'chart-bar']
-      }
-    },
-    'burn': {
-      component: FontAwesomeIcon,
-      props: {
-        icon: ['fas', 'burn']
-      }
-    },
-    'question': {
-      component: FontAwesomeIcon,
-      props: {
-        icon: ['fas', 'question']
-      }
-    },
-    'cog': {
-      component: FontAwesomeIcon,
-      props: {
-        icon: ['fas', 'cog']
-      }
-    }
-  }
+  iconfont: 'faSvg'
 })
