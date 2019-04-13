bc177ce839a05c6b6d29f4c00bd6c267728b7e9c
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-04-14 01:28:48 +0800

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,13 @@ import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAtom, faBalanceScale, faFlask, faChartBar, faBurn, faQuestion, faCog } from '@fortawesome/free-solid-svg-icons'
+import { faAtom } from '@fortawesome/free-solid-svg-icons/faAtom'
+import { faBalanceScale } from '@fortawesome/free-solid-svg-icons/faBalanceScale'
+import { faFlask } from '@fortawesome/free-solid-svg-icons/faFlask'
+import { faChartBar } from '@fortawesome/free-solid-svg-icons/faChartBar'
+import { faBurn } from '@fortawesome/free-solid-svg-icons/faBurn'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons/faQuestion'
+import { faCog } from '@fortawesome/free-solid-svg-icons/faCog'
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 library.add(faAtom, faBalanceScale, faFlask, faChartBar, faBurn, faQuestion, faCog)
