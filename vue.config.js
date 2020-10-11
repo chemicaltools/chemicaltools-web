7116ddfd9fd509e562d1eeee3d60a57adb272a91
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-10-11 14:35:20 -0400

    chore: use cdn.jsdelivr.net

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
           { name: 'vue-router', var: 'VueRouter', path: 'dist/vue-router.min.js' },
           { name: '@fortawesome/fontawesome-free', style: 'css/all.min.css', cssOnly: true }
         ],
-        prodUrl: "//js.njzjz.win/npm/:name@:version/:path"
+        prodUrl: "//cdn.jsdelivr.net//npm/:name@:version/:path"
         // publicPath: './node_modules'
       })
     ]
