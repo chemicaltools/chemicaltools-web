c0fb1933b41551e5ceedee8321b804df5e6e1ebf
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-10-12 20:46:05 -0400

    add cdn modules

--- vue.config.js
+++ vue.config.js
@@ -21,6 +21,9 @@ module.exports = {
           { name: 'vue-router', var: 'VueRouter', path: 'dist/vue-router.min.js' },
           { name: '@fortawesome/fontawesome-free', style: 'css/all.min.css', cssOnly: true },
           { name: 'chemicaltools', path: 'dist/main.js' },
+          { name: 'string-format', path: 'index.min.js' },
+          { name: 'vue-i18n', path: 'dist/vue-i18n.min.js' },
+          { name: 'vue2-storage', path: 'dist/vue2-storage.min.js' },
         ],
 		prod: process.env.CDN == 'yes',
         prodUrl: "//cdn.jsdelivr.net/npm/:name@:version/:path"
