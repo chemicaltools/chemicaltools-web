91463bfa43358e8dba63621c48834e60d1294dd9
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-10-27 23:14:17 -0400

    vuetify/lib?

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
           { name: 'string-format', var: 'format', path: 'index.min.js' },
           { name: 'vue-i18n', var:'VueI18n', path: 'dist/vue-i18n.min.js' },
           { name: 'vue2-storage', var: 'Vue2Storage', path: 'dist/vue2-storage.min.js' },
-          { name: 'vuetify', var: 'Vuetify', path: 'dist/vuetify.js', style: 'dist/vuetify.min.css' },
+          { name: 'vuetify/lib', var: 'Vuetify', path: 'dist/vuetify.js', style: 'dist/vuetify.min.css' },
         ],
         prodUrl: "//cdn.jsdelivr.net/npm/:name@:version/:path"
         // publicPath: './node_modules'
