d9be4a28019be8479a7b8bb170a48aaf892a9ec6
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-03 05:07:04 -0500

    chore: minify vuetify bundlesize using treeshaking

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-/// #if process.env.CDN != 'yes'
-import 'vuetify/dist/vuetify.min.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-/// #endif
+//import Vuetify from 'vuetify'
+import Vuetify from 'vuetify/lib'
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// import { faAtom, faBalanceScale, faFlask, faChartBar, faBurn, faQuestion, faCog, faBars, faHome } from '@fortawesome/free-solid-svg-icons'
+// /// #if process.env.CDN != 'yes'
+//import 'vuetify/dist/vuetify.min.css'
+//import '@fortawesome/fontawesome-free/css/all.css'
+// /// #endif
 
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
-// library.add(faAtom, faBalanceScale, faFlask, faChartBar, faBurn, faQuestion, faCog, faBars, faHome)
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faAtom, faBalanceScale, faFlask, faChartBar, faBurn, faQuestion, faCog, faBars, faHome } from '@fortawesome/free-solid-svg-icons'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faAtom, faBalanceScale, faFlask, faChartBar, faBurn, faQuestion, faCog, faBars, faHome)
 Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
-    // iconfont: 'faSvg',
-    iconfont: 'fa'
+    iconfont: 'faSvg',
+    //iconfont: 'fa'
   },
   theme: { dark: false }
 })
