4f188faeb0179330f6bb97a957d30073e6a300cb
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-04-07 16:18:26 -0400

    fix vue-i18n cdn path

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
                   { name: 'vue-router', var: 'VueRouter', path: 'dist/vue-router.min.js' },
                   { name: '@fortawesome/fontawesome-free', style: 'css/all.min.css', cssOnly: true },
                   { name: 'chemicaltools', var: 'chemicaltools', path: 'dist/main.js' },
-                  { name: 'vue-i18n', var: 'VueI18n', path: 'dist/vue-i18n.min.js' },
+                  { name: 'vue-i18n', var: 'VueI18n', path: 'dist/vue-i18n.global.prod.js' },
                   { name: 'vue2-storage', var: 'Vue2Storage', path: 'dist/vue2-storage.min.js' }
                   // { name: 'vuetify', var: 'Vuetify', path: 'dist/vuetify.min.js', style: 'dist/vuetify.min.css' },
                   // { name: 'core-js-bundle', path: 'minified.js' },
