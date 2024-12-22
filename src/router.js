be61cc2810a7786d56774f484fc267a4780cda63
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-12-22 01:06:27 -0500

    fix VueRouter

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import index from './components/index.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
 export default new Router({
   routes: [
